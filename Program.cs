98f5b6f1e049c2077a891b9f8f515eb9cc1c17c4
minhthuan <minhthuan26042001@gmail.com>
2023-09-02 21:12:58 +0700

    Them DI service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
+using CVLookup_WebAPI.Services.AccountService;
+using CVLookup_WebAPI.Services.AccountUserService;
+using CVLookup_WebAPI.Services.CandidateService;
+using CVLookup_WebAPI.Services.CurriculumService;
+using CVLookup_WebAPI.Services.EmployerService;
+using CVLookup_WebAPI.Services.ExperienceService;
+using CVLookup_WebAPI.Services.JobAddressService;
+using CVLookup_WebAPI.Services.JobCareerService;
+using CVLookup_WebAPI.Services.JobFieldService;
+using CVLookup_WebAPI.Services.JobFormService;
+using CVLookup_WebAPI.Services.JobPositionService;
+using CVLookup_WebAPI.Services.RecruitmentCVService;
+using CVLookup_WebAPI.Services.RecruitmentService;
+using CVLookup_WebAPI.Services.RoleService;
+using CVLookup_WebAPI.Services.UserRoleService;
+using CVLookup_WebAPI.Services.UserService;
 using FirstWebApi.Models.Database;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +29,24 @@ builder.Services.AddDbContext<AppDBContext>(options =>
 
 // Add automapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Add service
+builder.Services.AddScoped<IAccountUserService, AccountUserService>();
+builder.Services.AddScoped<IAcountService, AccountService>();
+builder.Services.AddScoped<ICandidateService, CandidateService>();
+builder.Services.AddScoped<ICurriculumViateService, CurriculumVitaeService>();
+builder.Services.AddScoped<IEmployerService, EmployerService>();
+builder.Services.AddScoped<IExperienceService, ExperienceService>();
+builder.Services.AddScoped<IJobAddressService, JobAddressService>();
+builder.Services.AddScoped<IJobCareerService, JobCareerService>();
+builder.Services.AddScoped<IJobFieldService, JobFieldService>();
+builder.Services.AddScoped<IJobFormService, JobFormService>();
+builder.Services.AddScoped<IJobPositionService, JobPositionService>();
+builder.Services.AddScoped<IRecruitmentCVService, RecruitmentCVService>();
+builder.Services.AddScoped<IRecruitmentService, RecruitmentService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUserRoleService, UserRoleService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
