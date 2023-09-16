faf415ec3543008481f1be640c862b0bb6e12b0d
minhthuan <minhthuan26042001@gmail.com>
2023-09-16 23:55:07 +0700

    Add JobField function

---
+++ Controllers/JobFieldController.cs
@@ -0,0 +1,204 @@
+﻿using CVLookup_WebAPI.Models.Domain;
+using CVLookup_WebAPI.Models.ViewModel;
+using CVLookup_WebAPI.Services.JobFieldService;
+using CVLookup_WebAPI.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVLookup_WebAPI.Controllers
+{
+	[Route("api/v1/[controller]/")]
+	[ApiController]
+	public class JobFieldController : ControllerBase
+	{
+		private readonly IJobFieldService _jobFieldService;
+		private readonly ILogger<JobFieldController> _logger;
+
+		public JobFieldController(ILogger<JobFieldController> logger, IJobFieldService jobFieldService)
+		{
+			_jobFieldService = jobFieldService;
+			_logger = logger;
+		}
+		/// <summary>
+		/// Lấy tất cả lĩnh vực công việc
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet("get-all-job-field")]
+		public async Task<IActionResult> getAllJobField()
+		{
+			try
+			{
+				var result = await _jobFieldService.JobFieldList();
+				return Ok(new ApiResponse
+				{
+					Code = StatusCodes.Status200OK,
+					Success = true,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (Exception e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Message = e.Message,
+					Code = StatusCodes.Status500InternalServerError
+				});
+			}
+		}
+
+		/// <summary>
+		/// Tìm kiếm lĩnh vực bằng id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpGet("get-job-field-by-id")]
+		public async Task<IActionResult> getJobFieldById([FromQuery] string id)
+		{
+			try
+			{
+				var result = await _jobFieldService.GetJobFieldById(id);
+				return Ok(new ApiResponse
+				{
+					Success = true,
+					Code = StatusCodes.Status200OK,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (ExceptionReturn e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Code = e.Code,
+					Message = e.Message
+				});
+			}
+		}
+
+		/// <summary>
+		/// Tìm kiếm lĩnh vực bằng tên
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		[HttpGet("get-job-field-by-address")]
+		public async Task<IActionResult> getJobFieldByAddress([FromQuery] string name)
+		{
+			try
+			{
+				var result = await _jobFieldService.GetJobFieldByName(name);
+				return Ok(new ApiResponse
+				{
+					Success = true,
+					Code = StatusCodes.Status200OK,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (ExceptionReturn e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Code = e.Code,
+					Message = e.Message
+				});
+			}
+		}
+
+		/// <summary>
+		/// Thêm lĩnh vực công việc mới
+		/// </summary>
+		/// <param name="jobAddress"></param>
+		/// <returns></returns>
+		[HttpPost("add-job-field")]
+		public async Task<IActionResult> addJobField([FromBody] JobFieldVM jobAddress)
+		{
+			try
+			{
+				var result = await _jobFieldService.Add(jobAddress);
+				return Ok(new ApiResponse
+				{
+					Success = true,
+					Code = StatusCodes.Status200OK,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (ExceptionReturn e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Code = e.Code,
+					Message = e.Message
+				});
+			}
+		}
+
+		/// <summary>
+		/// Xoá lĩnh vực công việc
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpDelete("delete")]
+		public async Task<IActionResult> deleteJobField([FromQuery] string id)
+		{
+			try
+			{
+
+				var result = await _jobFieldService.Delete(id);
+				return Ok(new ApiResponse
+				{
+					Success = true,
+					Code = StatusCodes.Status200OK,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (ExceptionReturn e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Code = e.Code,
+					Message = e.Message
+				});
+			}
+		}
+
+		/// <summary>
+		/// Cập nhật lĩnh vực làm việc
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="jobAddressVM"></param>
+		/// <returns></returns>
+		[HttpPatch("update")]
+		public async Task<IActionResult> updateJobField([FromQuery] string id, [FromBody] JobFieldVM jobAddressVM)
+		{
+			try
+			{
+				var result = await _jobFieldService.Update(id, jobAddressVM);
+				return Ok(new ApiResponse
+				{
+					Success = true,
+					Code = StatusCodes.Status200OK,
+					Data = result,
+					Message = "Hoàn thành"
+				});
+			}
+			catch (ExceptionReturn e)
+			{
+				return Ok(new ApiResponse
+				{
+					Success = false,
+					Code = e.Code,
+					Message = e.Message
+				});
+			}
+		}
+
+	}
+}
--- Services/JobFieldService/JobFieldService.cs
+++ Services/JobFieldService/JobFieldService.cs
@@ -1,40 +1,181 @@
-﻿using CVLookup_WebAPI.Models.ViewModel;
+﻿using AutoMapper;
+using CVLookup_WebAPI.Models.Domain;
+using CVLookup_WebAPI.Models.ViewModel;
+using CVLookup_WebAPI.Utilities;
 using FirstWebApi.Models.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace CVLookup_WebAPI.Services.JobFieldService
 {
 	public class JobFieldService : IJobFieldService
 	{
 		private readonly AppDBContext _dbContext;
+		private readonly IMapper _mapper;
 
-		public JobFieldService(AppDBContext dbContext)
+		public JobFieldService(AppDBContext dbContext, IMapper mapper)
 		{
 			_dbContext = dbContext;
+			_mapper = mapper;
 		}
 
-		public Task<JobFieldVM> Add(JobFieldVM jobField)
+		public async Task<JobField> Add(JobFieldVM jobFieldVM)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				var jobField = _mapper.Map<JobField>(jobFieldVM);
+				var addressExisted = await _dbContext.JobField.Where(prop => prop.Field == jobField.Field).FirstOrDefaultAsync();
+				if (addressExisted != null)
+				{
+					throw new ExceptionReturn(400, "Thất bại. Tên địa điểm đã tồn tại");
+				}
+				var result = await _dbContext.JobField.AddAsync(jobField);
+				if (result.State.ToString() == "Added")
+				{
+					int saveState = await _dbContext.SaveChangesAsync();
+					if (saveState <= 0)
+					{
+						throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình lưu dữ liệu");
+					}
+					return jobField;
+				}
+				else
+				{
+					throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình thêm dữ liệu");
+				}
+
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(e.Code, e.Message);
+			}
 		}
 
-		public Task<JobFieldVM> Delete(string Id)
+		public async Task<JobField> Delete(string Id)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				if (Id == null)
+				{
+					throw new ExceptionReturn(400, "Thất bại. Truy vấn không hợp lệ");
+				}
+
+				var jobField = await _dbContext.JobField.Where(prop => prop.Id == Id).FirstOrDefaultAsync();
+				if (jobField == null)
+				{
+					throw new ExceptionReturn(404, "Thất bại. Không thể tìm thấy dữ liệu");
+				}
+
+				var result = _dbContext.JobField.Remove(jobField);
+				if (result.State.ToString() == "Deleted")
+				{
+					var saveState = await _dbContext.SaveChangesAsync();
+					if (saveState <= 0)
+					{
+						throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình lưu dữ liệu");
+					}
+					return jobField;
+				}
+				else
+				{
+					throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình xoá dữ liệu");
+				}
+
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(e.Code, e.Message);
+			}
 		}
 
-		public Task<JobFieldVM> GetAccountById(int id)
+		public async Task<JobField> GetJobFieldById(string id)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				if (id == null)
+				{
+					throw new ExceptionReturn(400, "Thất bại. Truy vấn không hợp lệ");
+				}
+
+				var result = await _dbContext.JobField.Where(prop => prop.Id == id).FirstOrDefaultAsync();
+				if (result == null)
+				{
+					throw new ExceptionReturn(404, "Thất bại. Không thể tìm thấy dữ liệu");
+				}
+				return result;
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(e.Code, e.Message);
+			}
 		}
 
-		public Task<List<JobFieldVM>> JobFieldList()
+		public async Task<JobField> GetJobFieldByName(string address)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				if (address == null)
+				{
+					throw new ExceptionReturn(400, "Thất bại. Truy vấn không hợp lệ");
+				}
+
+				var result = await _dbContext.JobField.Where(prop => prop.Field == address).FirstOrDefaultAsync();
+				if (result == null)
+				{
+					throw new ExceptionReturn(404, "Thất bại. Không thể tìm thấy dữ liệu");
+				}
+				return result;
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(e.Code, e.Message);
+			}
 		}
 
-		public Task<JobFieldVM> Update(string Id, JobFieldVM newJobField)
+		public async Task<List<JobField>> JobFieldList()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				var jobFieldList = await _dbContext.JobField.ToListAsync();
+				return jobFieldList;
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(500, e.Message);
+			}
+		}
+
+		public async Task<JobField> Update(string Id, JobFieldVM newJobFieldVM)
+		{
+			try
+			{
+				var jobField = await _dbContext.JobField.Where(prop => prop.Id == Id).FirstOrDefaultAsync();
+				if (jobField == null)
+				{
+					throw new ExceptionReturn(404, "Thất bại. Không thể tìm thấy dữ liệu");
+				}
+
+				jobField.Field = newJobFieldVM.Field;
+				var result = _dbContext.JobField.Update(jobField);
+				if (result.State.ToString() == "Modified")
+				{
+					int saveState = await _dbContext.SaveChangesAsync();
+					if (saveState <= 0)
+					{
+						throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình lưu dữ liệu");
+					}
+					return jobField;
+
+				}
+				else
+				{
+					throw new ExceptionReturn(500, "Thất bại. Có lỗi xảy ra trong quá trình cập nhật dữ liệu");
+				}
+
+			}
+			catch (ExceptionReturn e)
+			{
+				throw new ExceptionReturn(e.Code, e.Message);
+			}
 		}
 	}
 }
