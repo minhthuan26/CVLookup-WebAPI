13afcf6f1c958dd115f83ec012d887cc3f92777a
Lê Minh Thuận <minhthuan26042001@gmail.com>
2023-11-07 15:52:34 +0700

    update username trong VM

--- Models/ViewModel/EmployerVM.cs
+++ Models/ViewModel/EmployerVM.cs
@@ -5,10 +5,6 @@ namespace CVLookup_WebAPI.Models.ViewModel
 {
     public class EmployerVM : UserVM
     {
-        [Required(ErrorMessage = "{0} không được để trống")]
-        [Display(Name = "Tên nhà tuyển dụng")]
-        public string EmployerName { get; set; }
-
 		[Required(ErrorMessage = "{0} không được để trống")]
 		[Display(Name = "Địa chỉ")]
 		public string Address { get; set; }
