87ece48181e05075a508cfed5e9b8226fb31212b
minhthuan <minhthuan26042001@gmail.com>
2023-12-08 20:22:50 +0700

    fix lỗi đăng ký

--- Models/ViewModel/UserVM.cs
+++ Models/ViewModel/UserVM.cs
@@ -5,6 +5,9 @@ namespace CVLookup_WebAPI.Models.ViewModel
 {
 	public abstract class UserVM
 	{
+		[Required(ErrorMessage = "{0} không được để trống")]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
 		public string? PhoneNumber { get; set; }
 
 		public IFormFile? Avatar { get; set; }
